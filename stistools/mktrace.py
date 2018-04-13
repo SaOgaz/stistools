3aabdf5aaa6e84fed50a57f52d25fa7fe105e853
Nadia Dencheva <nadia.dencheva@gmail.com>
2018-04-13 11:13:32 -0400

    Use astropy in mktrace

--- stistools/mktrace.py
+++ stistools/mktrace.py
@@ -28,17 +28,19 @@ Simple example of running mktrace on a STIS file named 'file.fits':
 """
 
 
+import os.path
 import numpy as np
 from astropy.io import fits
-import os.path
+from astropy.modeling import models, fitting
 from scipy import signal
 from scipy import ndimage as ni
 
-from stsci.tools import gfit, linefit
+from stsci.tools import linefit
 from stsci.tools import fileutil as fu
 
-__version__ = '2.0.0'
-__vdate__ = '2017-03-20'
+
+__version__ = '3.0.0'
+__vdate__ = '2018-04-20'
 
 
 def mktrace(fname, tracecen=0.0, weights=None):
@@ -116,10 +118,10 @@ def mktrace(fname, tracecen=0.0, weights=None):
     tr.writeTrace(fname, sciline, refline, interp_trace,
                   trace1024, tr_ind, a2disp_ind)
 
-    #print 'time', time.time()-start
-    #the minus sign is for consistency withthe way x2d reports the rotation
-    print("Traces were rotated by {:.10f} degrees \n".format((-(sparams[1]-rparams[1])*180 / np.pi)))
-    print('trace is centered on row {:.10f}'.format(tr._a2center))
+    # the minus sign is for consistency withthe way x2d reports the rotation
+    print("Traces were rotated by {0} degrees \n".format(
+        - (sparams[1] - rparams[1]) * 180 / np.pi))
+    print('Trace is centered on row {}'.format(tr._a2center))
     return tr
 
 
@@ -137,10 +139,10 @@ def interp(y, n):
     """
     m = float(len(y))
     x = np.arange(m)
-    i = np.arange(n,dtype=np.float)
-    xx = i * (m-1)/n
-    xind = np.searchsorted(x, xx)-1
-    yy = y[xind]+(xx-x[xind])*(y[xind+1]-y[xind])/(x[xind+1]-x[xind])
+    i = np.arange(n, dtype=np.float)
+    xx = i * (m - 1) / n
+    xind = np.searchsorted(x, xx) - 1
+    yy = y[xind] + (xx - x[xind]) * (y[xind + 1] - y[xind]) / (x[xind + 1] - x[xind])
 
     return yy
 
@@ -216,7 +218,6 @@ class Trace:
         self.sptrctab = self.openTraceFile(fu.osfn(self.sptrctabname))
 
     def openTraceFile(self, filename):
-
         """
         Returns a spectrum trace table
         """
@@ -263,9 +264,7 @@ class Trace:
 
         return tr
 
-    def writeTrace(self, fname, sciline, refline, interp_trace, trace1024,
-                   tr_ind, a2disp_ind):
-
+    def writeTrace(self, fname, sciline, refline, interp_trace, trace1024, tr_ind, a2disp_ind):
         """
         The 'writeTrace' method performs the following steps:
 
@@ -352,13 +351,13 @@ class Trace:
 
         sizex, sizey = data.shape
         subim_size = 40
-        y1 = int(_tracecen - subim_size/2.)
-        y2 = int(_tracecen + subim_size/2.)
+        y1 = int(_tracecen - subim_size / 2.)
+        y2 = int(_tracecen + subim_size / 2.)
         if y1 < 0:
             y1 = 0
-        if y2 > (sizex -1):
+        if y2 > (sizex - 1):
             y2 = sizex - 1
-        specimage = data[y1:y2+1, :]
+        specimage = data[y1: y2 + 1, :]
         smoytrace = self.gFitTrace(specimage, y1, y2)
         yshift = int(np.median(smoytrace) - 20)
         y1 = y1 + yshift
@@ -367,7 +366,7 @@ class Trace:
             y1 = 0
         if y2 > sizex:
             y2 = sizex
-        specimage = data[y1:y2+1, :]
+        specimage = data[y1: y2 + 1, :]
         smoytrace = self.gFitTrace(specimage, y1, y2)
         med11smoytrace = ni.median_filter(smoytrace, 11)
         med11smoytrace[0] = med11smoytrace[2]
@@ -403,12 +402,17 @@ class Trace:
         sizex, sizey = specimage.shape
         smoytrace = np.zeros(sizey).astype(np.float)
         boxcar_kernel = signal.boxcar(3) / 3.0
+        fitter = fitting.LevMarLSQFitter()
 
         for c in np.arange(sizey):
             col = specimage[:, c]
             col = col - np.median(col)
             smcol = ni.convolve(col, boxcar_kernel).astype(np.float)
-            fit = gfit.gfit1d(smcol, quiet=1, maxiter=15)
-            smoytrace[c] = fit.params[1]
+            # fit = gfit.gfit1d(smcol, quiet=1, maxiter=15)
+            x = np.arange(len(smcol)).astype(np.float)
+            gauss = models.Gaussian1D(amplitude=smcol.max(), mean=x.mean())
+            fit = fitter(gauss, x, smcol)
+            # smoytrace[c] = fit.params[1]
+            smoytrace[c] = fit.mean.value
 
         return np.array(smoytrace)
